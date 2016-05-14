0d650b7dd8ba2b6bb7dc29b8fc910598dfc4ab7d
jayeshdesk <jayeshdesk@gmail.com>
2016-05-14 15:16:14 +0530

    initial commit

---
+++ app/src/main/java/com/testapp/vtz/birthdayapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.testapp.vtz.birthdayapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
